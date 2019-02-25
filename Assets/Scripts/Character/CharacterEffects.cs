f39b3e72773ad9f0b3a5a11dbe602cd13b5d38e9
fabros64 <32464588+fabros64@users.noreply.github.com>
2019-02-25 20:03:54 +0100

    awesome particle effects

--- Assets/Scripts/Character/CharacterEffects.cs
+++ Assets/Scripts/Character/CharacterEffects.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using BitStrap;
 using UnityEngine;
 
 public class CharacterEffects : MonoBehaviour
@@ -8,23 +9,38 @@ public class CharacterEffects : MonoBehaviour
 
     private void ParticleSetActive(bool state)
     {
+        Debug.Log(state);
         if (state)
         {
             ParticleSystem.Play();
         }
         else
         {
-            if(!MyKeyCode.Left.IsKeyHeld() && !MyKeyCode.Right.IsKeyHeld())
-            {
-                ParticleSystem.Stop();
-            }
+            ParticleSystem.Stop();
+        }
+    }
+
+    private void RotateChange(MyKeyCode key)
+    {
+
+        if (key.Equals(MyKeyCode.Left))
+            ParticleSystem.shape.rotation.Set(0,0,75);
+        else
+        {
+            ParticleSystem.shape.rotation.Set(0, 0, -75);
         }
     }
 
     private void Start()
     {
+        //STF.InputController.RegisterKeyAction(MyKeyCode.Left, true, () => ParticleSystem.shape.rotation.Set(0, 0, 75));
+        STF.InputController.RegisterKeyAction(MyKeyCode.Left, true, () => RotateChange(MyKeyCode.Left));
         STF.InputController.RegisterKeyAction(MyKeyCode.Left, true, () => ParticleSetActive(true));
+
+        //STF.InputController.RegisterKeyAction(MyKeyCode.Right, true, () => ParticleSystem.shape.rotation.Set(0, 0, -75));
         STF.InputController.RegisterKeyAction(MyKeyCode.Right, true, () => ParticleSetActive(true));
+        STF.InputController.RegisterKeyAction(MyKeyCode.Left, true, () => RotateChange(MyKeyCode.Right));
+
 
         STF.InputController.RegisterKeyAction(MyKeyCode.Left, false, () => ParticleSetActive(false));
         STF.InputController.RegisterKeyAction(MyKeyCode.Right, false, () => ParticleSetActive(false));
