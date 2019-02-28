ca1b157e1a5edc9ad5dc0d96a8ac518ce0c8ab30
Sebastian Ciba <sebastian.ciba@o2.pl>
2019-02-28 12:51:02 +0100

    granade and bullet

---
+++ Assets/Scripts/Enemies/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float power = 9.0f;
+    public Rigidbody2D bullet;
+
+    void Start()
+    {
+        Vector2 vec = new Vector2(-0.02f, 0.002f);
+        bullet.AddForce(vec * power, ForceMode2D.Impulse);
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(bullet.gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemies/Granade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Granade : MonoBehaviour
+{
+    private float power = 3.0f;
+    private int timeCounter;
+    private int timeLimit = 120;
+    public Rigidbody2D granade;
+
+    void Start()
+    {
+        Vector2 vec = new Vector2(-0.2f, 0.1f);
+        granade.AddForce(vec * power, ForceMode2D.Impulse);
+    }
+
+    void Update()
+    {
+        timeCounter++;
+        if (timeCounter > timeLimit)
+        {
+            granade.gameObject.SetActive(false);
+            Destroy(granade.gameObject);
+        }
+
+    }
+}
